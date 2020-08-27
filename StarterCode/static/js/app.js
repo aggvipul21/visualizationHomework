8f8fca297314511c72a1dcc2bbfdf3267bb2f430
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-27 16:57:03 -0400

    added code to fill data in dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,4 +3,26 @@ const url = "../data/samples.json";
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log(data);
-  });
+    // console.log(data.samples.map(sample=>sample));  
+    // console.log(data.samples.map(sample=>sample.id));  
+    // console.log(data.samples.map(sample=>sample.otu_ids)); 
+    // console.log(data.names.map(name=>name));  
+
+    var dropDown=d3.select("#selDataset")
+
+    var options=dropDown.selectAll("option")
+    .data(data.names)
+    .enter()
+    .append("option");
+
+    // console.log("1")
+    
+    options.text(function(d) {
+    //   console.log(d);
+      return d;
+    })
+    .attr("value", function(d) {
+      return d;
+    });
+
+});
