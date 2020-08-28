8ea59c5446faee3ebe777a4619c3073f5055c8ff
Vipul Aggarwal <aggarwal.vipul21@gmail.com>
2020-08-28 08:33:50 -0400

    Changed logic to build bubble plot- Use fileteredsampledata calculated using filtereddata function instead of filtering data in bubble plot

--- static/js/app.js
+++ static/js/app.js
@@ -58,93 +58,70 @@ d3.json(url).then(function(data) {
         var filteredsample=data.samples.filter(filterdata.bind(this, dataset));
         console.log(filteredsample);
 
-        //Bar plot for sample data filtered based on selection of drop-down value
-        //Setting up trace for plot
-        var trace1={
-            type: 'bar',
-            //Only first 10 sample values in reverse order needed
-            x: value.sample_values.slice(0,9).reverse(),
-            //OOnly first 10 Otu_ids in reverse order needed
-            y: value.otu_ids.slice(0,9).reverse(),
-            //Orientation set to 'h' as it is horizontal bar
-            orientation: 'h',
-            //Hovertext set to otu_labels
-            hovertext:value.otu_labels.slice(0,9)
-        }
-        
-        //Setting data for plot
-        var data=[trace1];
-        
-        //Setting layout for plot
-        var layout = {
-            yaxis:
-            {
-                //Adding 'OTU ' as prefix for ticker for y-axis
-                tickprefix: "OTU ",
-                //Setting y-axis value's type as categories to plot in order of data and not as values on y-axis
-                type: 'category'
-            }
-        };
-
-        //Plot bar using data and layout set above
-        Plotly.newPlot('bar', data,layout);
-
-
-        Object.entries(data.samples).forEach(([key,value])=>{
+        //Plot bars using filtered sample data
+        Object.entries(filteredsample).forEach(([key,value])=>{
             
-            if (value.id===dataset){
-
-                // var trace1={
-                //     type: 'bar',
-                //     x: value.sample_values.slice(0,9).reverse(),
-                //     y: value.otu_ids.slice(0,9).reverse(),
-                //     orientation: 'h',
-                //     hovertext:value.otu_labels.slice(0,9)
-                // }
-                
-                // var data=[trace1];
-                
-                // var layout = {
-                //     yaxis:
-                //     {
-                //         tickprefix: "OTU ",
-                //         type: 'category'
-                //     }
-                // };
-
-
-                // Plotly.newPlot('bar', data,layout);
-
-                var bubble_trace = {
-                    x: value.otu_ids,
-                    y: value.sample_values,
-                    text:value.otu_labels,
-                    mode: 'markers',
-                    marker: {
-                      size: value.sample_values,
-                      colorscale: [
-                        ['0.0', 'rgb(255,255,0)'],
-                        ['0.5','rgb(0,255,85)'],
-                        ['1.0', 'rgb(255,25,179)']
-                      ],
-                      color: value.otu_ids
-                    }
-                  };
-                  
-                  var data = [bubble_trace];
-                  
-                Plotly.newPlot('bubble', data);
-
-               }
+            //Bar plot for sample data filtered based on selection of drop-down value
+            //Setting up trace for plot
+            var trace1={
+                type: 'bar',
+                //Only first 10 sample values in reverse order needed
+                x: value.sample_values.slice(0,9).reverse(),
+                //Only first 10 Otu_ids in reverse order needed
+                y: value.otu_ids.slice(0,9).reverse(),
+                //Orientation set to 'h' as it is horizontal bar
+                orientation: 'h',
+                //Hovertext set to otu_labels
+                hovertext:value.otu_labels.slice(0,9)
+            }            
+            //Setting data for plot
+            var data=[trace1];
             
+            //Setting layout for plot
+            var layout = {
+                yaxis:
+                {
+                    //Adding 'OTU ' as prefix for ticker for y-axis
+                    tickprefix: "OTU ",
+                    //Setting y-axis value's type as categories to plot in order of data and not as values on y-axis
+                    type: 'category'
+                }
+            };
+
+            //Plot bar using data and layout set above
+            Plotly.newPlot('bar', data,layout);
+
+            //Bubble plot for sample data filtered based on selection of drop-down value
+            //Setting up trace for plot
+            var bubble_trace = {
+                //Otu_Ids on x-axis
+                x: value.otu_ids,
+                //Sample Values on y-axis
+                y: value.sample_values,
+                //otu_labels as text shown on hover
+                text:value.otu_labels,
+                //mode as markers
+                mode: 'markers',
+                marker: {
+                    //Sample_values as size of bubbles
+                    size: value.sample_values,
+                    //Setting color scale from yellow to pink with green in the middle
+                    colorscale: [
+                    ['0.0', 'rgb(255,255,0)'],
+                    ['0.5','rgb(0,255,85)'],
+                    ['1.0', 'rgb(255,25,179)']
+                    ],
+                    //Otu_ids as color of bubbles
+                    color: value.otu_ids
+                }
+            };
+            
+            //Setting data for plot
+            var data = [bubble_trace];
+            
+            //Plot bubble chart using data and layout set above
+            Plotly.newPlot('bubble', data);
         });
-      
-       
-
-
-
-
     }
-
 });
 
